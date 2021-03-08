44a98dab775dbe137a45d6835cf9a51236a13590
Okky Mabruri <okkymabrur@gmail.com>
2021-03-08 14:58:42 +0700

    :zap: Bump New Version

--- README.md
+++ README.md
@@ -1,54 +1,28 @@
 # Putusan Pilkada Mahkamah Agung Indonesia DataScraper
 
 
-Program ini digunakan untuk scraping data pelanggaran pilkada di website [https://putusan3.mahkamahagung.go.id/search.html?q=pilkada](https://putusan3.mahkamahagung.go.id/search.html?q=pilkada)
+Program ini digunakan untuk scraping data di website [https://putusan3.mahkamahagung.go.id/search.html](https://putusan3.mahkamahagung.go.id/search.html)
 
 Silahkan digunakan / dicopy / dimodifikasi agar menjadi bermanfaat.
 
-Tested di Ubuntu 20.04
+Tested di Ubuntu 20.10
 
 
-![scraping1](https://github.com/okkymabruri/putusan-pilkada-scraping/raw/master/image/1.png)
+![scraping1](./img/1.png)
 
-![scraping2](https://github.com/okkymabruri/putusan-pilkada-scraping/raw/master/image/2.png)
+![scraping2](./img/2.png)
+
+## Demo
+![scraping2](./img/demo.gif)
 
 ## Prerequisites
 
-#### Update dan Install Package
-```
-sudo apt-get update
-sudo apt-get install -y unzip xvfb libxi6 libgconf-2-4
-```
-#### Install Oracle Java 8
-```
-sudo apt-get install default-jdk
-```
-#### Install Google Chrome
-```
-sudo curl -sS -o - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add
-sudo echo "deb [arch=amd64]  http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list
-sudo apt-get -y update
-sudo apt-get -y install google-chrome-stable
-```
-#### Instalasi Chrome Driver
-```
-wget https://chromedriver.storage.googleapis.com/87.0.4280.88/chromedriver_linux64.zip
-unzip chromedriver_linux64.zip
-sudo mv chromedriver /usr/local/bin
-sudo chown root:root /usr/local/bin/chromedriver
-sudo chmod +x /usr/local/bin/chromedriver
-```
-#### Install Selenium
-```
-pip install selenium
-```
-
 ## Menjalankan Program
 ```
-git clone https://github.com/okkymabruri/putusan-pilkada-scraping.git
-cd putusan-pilkada-scraping/
+git clone https://github.com/okkymabruri/putusan-mahkamahagung
+cd putusan-mahkamahagung/
 pip install -r requirements.txt
-python3 scraping.py
+python putusan-ma.py -p https://putusan3.mahkamahagung.go.id/search.html?cat=98821d8a4bc63aff3a81f66c37934f56 -o putusanUUITE
 ```
 
 ## Sumber
--- putusan-ma.py
+++ putusan-ma.py
@@ -1,126 +1,126 @@
-#!/usr/bin/env python
+#################################################################################
+# author: Okky Mabruri
+# email: okkymabrur@gmail.com
+# usage: python putusan-ma.py -p https://putusan3.mahkamahagung.go.id/search.html?cat=98821d8a4bc63aff3a81f66c37934f56 -o putusanUUITE
+#################################################################################
+
+import argparse
 
 # Importing packages
-from selenium import webdriver # webdriver from selenium framework to load web
-import pandas as pd # pandas library fpr data processing
-import time # time library to manipulate time (pause, etc)
-import datetime
+import datetime  # module datetime for dealing with dates and times
+import re  # regex
+import time  # time library to manipulate time (pause, etc)
+
+import pandas as pd  # pandas library fpr data processing
+from bs4 import BeautifulSoup
+from selenium import webdriver  # webdriver from selenium framework to load web
+from selenium.webdriver.common.keys import Keys
+from webdriver_manager.chrome import ChromeDriverManager
+
+parser = argparse.ArgumentParser(
+    description="Putusan Mahkamah Agung Scraper", add_help=True
+)
+parser.add_argument(
+    "-p",
+    "--page",
+    required=True,
+    dest="page",
+    help="Masukkan link pencarian putusan pilkada",
+)
+parser.add_argument(
+    "-o",
+    "--output",
+    required=False,
+    dest="file_name",
+    default="output",
+    help="masukkan nama output file, misal UUITE",
+)
+
+parser.add_argument(
+    "-he",
+    "--headless",
+    required=False,
+    dest="headless",
+    default=False,
+    help="masukkan nama output file, misal UUITE",
+)
+
+args = parser.parse_args()
+page = args.page
+file_name = args.file_name
+headless = args.headless
+
+def runbrowser(headless):
+    options = webdriver.ChromeOptions()
+    options.headless = headless
+    options.add_argument("--incognito")
+    options.add_argument("--start-maximized")
+    return webdriver.Chrome(ChromeDriverManager().install(), options=options)
+
+
+# get last page
+def get_last_page():
+    html = driver.page_source
+    soup = BeautifulSoup(html, "html.parser")
+    last_page = soup.find_all("a", attrs={"class": "page-link"})[-1][
+        "data-ci-pagination-page"
+    ]
+    return last_page
+
+
+def get_link():
+    html = driver.page_source
+    soup = BeautifulSoup(html, "html.parser")
+    links = [
+        a.get("href")
+        for a in soup.find_all(
+            "a",
+            attrs={
+                "href": re.compile(
+                    "https://putusan3.mahkamahagung.go.id/direktori/putusan/"
+                )
+            },
+        )
+    ]
+    return links
+
+
+def get_data(link):
+    driver.get(link)
+    html = driver.page_source
+    soup = BeautifulSoup(html, "html.parser")
+    jumlahview = soup.find("div", attrs={"title": "Jumlah view"}).get_text()
+    jumlahdl = soup.find("div", attrs={"title": "Jumlah download"}).get_text()
+    table = soup.find("table")
+    df = pd.read_html(str(table))[0]
+    df = df.T
+    df.columns = df.iloc[0]
+    df = df.rename(columns={df.columns[0]: "Judul"})
+    df = df[1:]
+    df["Jumlah View"] = jumlahview
+    df["Jumlah Download"] = jumlahdl
+    df["Link"] = link
+    return df
+
+
+driver = runbrowser(headless)
+# page = "https://putusan3.mahkamahagung.go.id/search.html?cat=98821d8a4bc63aff3a81f66c37934f56"
+page = page + "&obf=TANGGAL_PUTUS&obm=desc"  # sort by tanggal putusan
+driver.get(page)
 
 timenow = datetime.datetime.now().strftime("%m-%d-%Y")
-# Webdriver setup, you can use Chrome or Firefor
-options = webdriver.ChromeOptions()
-# options = webdriver.FirefoxOptions()  # if use firefox (firefox fast than chrome but more inconsistency
-options.add_argument("disable-infobars") # add option to disable info bar
-options.add_argument("--disable-extensions") # add option to disable browser extension
-driver = webdriver.Chrome(chrome_options=options, executable_path='/usr/local/bin/chromedriver') # load driver chrome
-# driver = webdriver.Firefox(firefox_options=options, executable_path='/usr/local/bin/geckodriver')
-driver.delete_all_cookies()
+last_page = get_last_page()
 
-# Create empty dataframe
-data = pd.DataFrame(columns=[
-    'Nomor',
-    'ParaPihak',
-    'TingkatProses',
-    'Klasifikasi',
-    'Tahun',
-    'TanggalRegister',
-    'LembagaPeradilan',
-    'JenisLembagaPeradilan',
-    'HakimKetua',
-    'HakimAnggota',
-    'Panitera',
-    'Amar',
-    'CatatanAmar',
-    'TanggalMusyawarah',
-    'TanggalDibacakan',
-    'Kaidah',
-    'Status',
-    'Laman'])
+result = pd.DataFrame(None)
+for i in range(1, int(last_page)+1):
+    print("=========== Scraping Page " + str(i) + "===========")
+    driver.get(page + "&page=" + str(i))
+    links = get_link()
+    for link in links:
+        df = get_data(link)
+        result = result.append(df)
+    print("Total data: " + str(result.shape[0]))
 
-# Function for handle error & more flexible
-def safe_execute(function):
-    value = []
-    getvalue = []
-    while True:
-        try:
-            value = function
-            getvalue = value[0].text
-            return getvalue
-        except IndexError:
-            pass
-        return getvalue
-
-
-link = []
-for z in range(1, 23):
-    link.append('//section/div/div[2]/div[' + str(z) + ']/div/strong/a')
-
-# Open each page, putusan3.mahkamahagung.go.id/search.html?q=pilkada has 138 pages
-for i in range(1, 138):
-    page = 'https://putusan3.mahkamahagung.go.id/search.html?q=pilkada&page='
-    driver.get(page + str(i))
-
-    # Get url, each pages contain 19-22 pages
-    url = []
-    for j in range(22):
-        items = driver.find_elements_by_xpath(link[j])
-        for item in items:
-            href = item.get_attribute('href')
-            url.append(href)
-    print('Page ' + str(i))
-
-    # Open each url
-    for k in range(len(url)):
-        driver.get(url[k])
-        time.sleep(1)
-
-        Xpath = '//*[@id="popular-post-list-sidebar"]/ul/table/tbody/'
-
-        # Get data from each url
-        Nomor = safe_execute(driver.find_elements_by_xpath(Xpath + 'tr[1]/td[2]'))
-        ParaPihak = safe_execute(driver.find_elements_by_xpath(Xpath + 'tr[2]/td[2]'))
-        TingkatProses = safe_execute(driver.find_elements_by_xpath(Xpath + 'tr[3]/td[2]'))
-        Klasifikasi = safe_execute(driver.find_elements_by_xpath(Xpath + 'tr[4]/td[2]'))
-        Tahun = safe_execute(driver.find_elements_by_xpath(Xpath + 'tr[5]/td[2]'))
-        TanggalRegister = safe_execute(driver.find_elements_by_xpath(Xpath + 'tr[6]/td[2]'))
-        LembagaPeradilan = safe_execute(driver.find_elements_by_xpath(Xpath + 'tr[7]/td[2]'))
-        JenisLembagaPeradilan = safe_execute(driver.find_elements_by_xpath(Xpath + 'tr[8]/td[2]'))
-        HakimKetua = safe_execute(driver.find_elements_by_xpath(Xpath + 'tr[9]/td[2]'))
-        HakimAnggota = safe_execute(driver.find_elements_by_xpath(Xpath + 'tr[10]/td[2]'))
-        Panitera = safe_execute(driver.find_elements_by_xpath(Xpath + 'tr[11]/td[2]'))
-        Amar = safe_execute(driver.find_elements_by_xpath(Xpath + 'tr[12]/td[2]'))
-        CatatanAmar = safe_execute(driver.find_elements_by_xpath(Xpath + 'tr[13]/td[2]'))
-        TanggalMusyawarah = safe_execute(driver.find_elements_by_xpath(Xpath + 'tr[14]/td[2]'))
-        TanggalDibacakan = safe_execute(driver.find_elements_by_xpath(Xpath + 'tr[15]/td[2]'))
-        Kaidah = safe_execute(driver.find_elements_by_xpath(Xpath + 'tr[16]/td[2]'))
-        Status = safe_execute(driver.find_elements_by_xpath(Xpath + 'tr[17]/td[2]'))
-        Laman = url[k]
-
-
-        # Add new data in new rows
-        data = data.append({
-            'Nomor': Nomor,
-            'ParaPihak': ParaPihak,
-            'TingkatProses': TingkatProses,
-            'Klasifikasi': Klasifikasi,
-            'Tahun': Tahun,
-            'TanggalRegister': TanggalRegister,
-            'LembagaPeradilan': LembagaPeradilan,
-            'JenisLembagaPeradilan': JenisLembagaPeradilan,
-            'HakimKetua': HakimKetua,
-            'HakimAnggota': HakimAnggota,
-            'Panitera': Panitera,
-            'Amar': Amar,
-            'CatatanAmar': CatatanAmar,
-            'TanggalMusyawarah': TanggalMusyawarah,
-            'TanggalDibacakan': TanggalDibacakan,
-            'Kaidah': Kaidah,
-            'Status': Status,
-            'Laman': Laman}, ignore_index=True)
-
-    print(data)
-
-    # Export to 'output.xlsx'
-    data.to_excel("./output" + str(timenow) + ".xlsx", index=Fasle)
-
+# Export to '.csv'
+result.to_csv(file_name + str(timenow) + ".csv", index=False)
 driver.quit()
